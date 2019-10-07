9c2855ed43fdfe1cb35173a5c268e038a94a6fb2
Alfred Wong <alfred.cl.wong@gmail.com>
2019-10-07 14:07:59 +0100

    added captions

--- projects.php
+++ projects.php
@@ -9,21 +9,49 @@
   <main>
     <div>
       <h2>School</h2>
-      <img src="images/electronics.jpg" alt="Mobile Audio System" height="300px" style="" align="top">
-      <img src="images/image50.jpg" height="300px">
+      <figure>
+        <img src="images/electronics.jpg" alt="Mobile Audio System" height="300px">
+        <figcaption>Mobile Audio System</figcaption>
+      </figure>
+      <figure>
+        <img src="images/image50.jpg" alt="Quadcopter Control" height="300px">
+        <figcaption>Quadcopter Control</figcaption>
+      </figure>
     </div>
+    <br>
+    <hr width=90%>
     <div>
       <h2>Mathematical Tripos (CATAM)</h2>
-      <img src="images/ODEs.jpg" height="200px">
-      <img src="images/gss.jpg" height="200px">
-      <img src="images/3bp.jpg" height="200px">
-      <img src="images/crypt.jpg" height="200px">
+      <figure>
+        <img src="images/ODEs.jpg" height="200px">
+        <figcaption>Ordinary Differential Equations</figcaption>
+      </figure>
+      <figure>
+        <img src="images/gss.jpg" height="200px">
+        <figcaption>Golden Section Search</figcaption>
+      </figure>
+      <figure>
+        <img src="images/3bp.jpg" height="200px">
+        <figcaption>Three Body Problem</figcaption>
+      </figure>
+      <figure>
+        <img src="images/crypt.jpg" height="200px">
+        <figcaption>Public-Key Cryptography</figcaption>
+      </figure>
     </div>
+    <br>
+    <hr width=90%>
     <div>
-      <h2>Miscellaneous</h2>
-      <img src="images/waterscope-improc.png" height="300px">
+      <h2>Industry</h2>
+      <figure>
+        <img src="images/waterscope-improc.png" height="300px">
+        <figcaption>CFU Tracker for Water Testing</figcaption>
+      </figure>
       <!--img src="images/chess.jpg" height="300px"-->
-      <img src="images/apv-mcts.png" height=300px">
+      <figure>
+        <img src="images/apv-mcts.png" height=300px">
+        <figcaption>APV-MCTS for a newly released TCG</figcaption>
+      </figure>
     </div>
   </main>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
