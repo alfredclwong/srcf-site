95a581ca25ed6cf91a4fc6c26e3aa5af44f5cc3d
Alfred Wong <alfred.cl.wong@gmail.com>
2019-10-07 13:03:10 +0100

    added hover functionality to project images

--- projects.php
+++ projects.php
@@ -7,15 +7,33 @@
 <body>
 <?php include("navbar.inc.php"); ?>
   <main>
-    <img src="images/electronics.jpg" height="300px">
-    <img src="images/image50.jpg" height="300px">
-    <img src="images/waterscope-improc.png" height="300px">
-    <img src="images/chess.jpg" height="300px">
-    <img src="images/apv-mcts.png" height=300px">
-    <img src="images/ODEs.jpg" height="300px">
-    <img src="images/gss.jpg" height="300px">
-    <img src="images/3bp.jpg" height="300px">
-    <img src="images/crypt.jpg" height="300px">
+    <div>
+      <h2>School</h2>
+      <a href="#" target="blank">
+        <div class="hoverbox">
+          <img src="images/electronics.jpg" alt="Mobile Audio System" class="hoverbox-layer_bottom" height="300px">
+          <div class="hoverbox-layer_top">
+            <div class="hoverbox-text">2014<br><br>GCSE Electronics Project<br><br>Mobile Audio System<div class="hoverbox-text_mobile">Tap again to follow link</div></div>
+          </div>
+        </div>
+      </a>
+      <img src="images/image50.jpg" height="300px">
+    </div>
+    <div>
+      <h2>Mathematical Tripos (CATAM)</h2>
+      <img src="images/ODEs.jpg" height="200px">
+      <img src="images/gss.jpg" height="200px">
+      <img src="images/3bp.jpg" height="200px">
+      <img src="images/crypt.jpg" height="200px">
+    </div>
+    <div>
+      <h2>Miscellaneous</h2>
+      <img src="images/waterscope-improc.png" height="300px">
+      <!--img src="images/chess.jpg" height="300px"-->
+      <img src="images/apv-mcts.png" height=300px">
+    </div>
   </main>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+  <script src="src/hover-box.js"></script>
 </body>
 </html>
